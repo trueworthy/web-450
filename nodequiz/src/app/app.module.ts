83a20d151e28b2b4bd43c8695f9b75123dcdc666
Lea <ltrueworthy@yahoo.com>
2019-09-18 16:06:56 -0700

    week 1

---
+++ nodequiz/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
+//import {HttpClientModule, HttpClient} from ''
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BaseLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

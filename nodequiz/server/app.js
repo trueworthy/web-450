35aa305b723acad600b82f8179eb1e7ca26e535a
Lea <ltrueworthy@yahoo.com>
2019-10-15 22:27:35 -0700

    adding engines and postinsatll

--- nodequiz/server/app.js
+++ nodequiz/server/app.js
@@ -23,7 +23,8 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../dist/nodequiz')));
 app.use('/', express.static(path.join(__dirname, '../dist/nodequiz')));
 
-const serverPort = 3000; // port the application listens on
+const port = process.env.PORT || 3000, //server port or 3000
+//const serverPort = 3000; // port the application listens on
 
 // MongoDB (mLab) connection string
 // const connString = 'mongodb://<username>:<password>@<host-name>:<port><database-name>';
